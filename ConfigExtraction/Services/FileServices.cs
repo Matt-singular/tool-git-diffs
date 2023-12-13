c644f6dd51a49091c6726caf351616c72a80c71b
Matthew Lewis <mlewis@singular.co.za>
2023-12-13 22:52:25 +0200

    Extracted the file reading logic to a service to allow for unit test mocking

---
+++ ConfigExtraction/Services/FileServices.cs
@@ -0,0 +1,38 @@
+﻿namespace ConfigExtraction.Services;
+
+using System;
+
+/// <summary>
+/// Various file services
+/// </summary>
+public class FileServices : IFileServices
+{
+  public string GetFullPath(string fileName = "config.json")
+  {
+    var basePath = Path.Combine(AppContext.BaseDirectory);
+    var configFilePath = Path.Combine(basePath, fileName);
+
+    return configFilePath;
+  }
+
+  public bool Exists(string path)
+  {
+    var fileExists = File.Exists(path);
+
+    return fileExists;
+  }
+
+  public string ReadText(string path)
+  {
+    var fileContents = File.ReadAllText(path);
+
+    return fileContents ?? string.Empty;
+  }
+}
+
+public interface IFileServices
+{
+  public string GetFullPath(string fileName = "config.json");
+  public bool Exists(string path);
+  public string ReadText(string path);
+}
