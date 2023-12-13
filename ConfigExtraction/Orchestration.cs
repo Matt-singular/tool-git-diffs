d7524b87cc22f06b477335ec1dd808b61319b833
Matthew Lewis <mlewis@singular.co.za>
2023-12-13 22:53:35 +0200

    Renamed the ConfigExtraction Orchestration logic to avoid namespace clashes

---
+++ ConfigExtraction/Orchestration.cs
@@ -0,0 +1,27 @@
+﻿namespace ConfigExtraction;
+
+using Services;
+
+/// <summary>
+/// Orchestration of all config extraction related logic
+/// </summary>
+public class Orchestration : IOrchestration
+{
+  private readonly IReadConfig readConfig;
+
+  public Orchestration(IReadConfig readConfig)
+  {
+    this.readConfig = readConfig;
+  }
+
+  public void Process()
+  {
+    // 1) Read in the config.json file
+    var configContents = this.readConfig.Process();
+  }
+}
+
+public interface IOrchestration
+{
+  public void Process();
+}
