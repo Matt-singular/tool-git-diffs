1f2a7eebba2c37eb347b644eb12563d02021d951
Matthew Lewis <mlewis@singular.co.za>
2023-12-15 13:44:22 +0200

    Updated the FileServices to consist with other services, improve testability and code documentation

--- ConfigExtraction/Services/FileServices.cs
+++ ConfigExtraction/Services/FileServices.cs
@@ -7,6 +7,11 @@ using System;
 /// </summary>
 public class FileServices : IFileServices
 {
+  /// <summary>
+  /// Sets the full file path using the provided file name
+  /// </summary>
+  /// <param name="fileName">The name of the file</param>
+  /// <returns>The full file path in string format</returns>
   public string GetFullPath(string? fileName = null)
   {
     // Set the default file name
@@ -27,18 +32,22 @@ public class FileServices : IFileServices
   /// If developing locally, then debug will be enabled
   /// </summary>
   /// <returns>The defaulted file name</returns>
-  private string SetDefaultFileName()
+  public virtual string SetDefaultFileName()
   {
-    var defaultFileName = "config.json";
 #if DEBUG
     // On our local the file is nested under the Files folder for tidiness
-    return $"Files\\{defaultFileName}";
+    return Constants.defaultFileNameDebugMode;
 #else
     // When using the published exe the file will be inline with the exe
-    return defaultFileName;
+    return Constants.defaultFileNameReleaseMode;
 #endif
   }
 
+  /// <summary>
+  /// Checks if the file exists
+  /// </summary>
+  /// <param name="path">The path of the file to check</param>
+  /// <returns>True if the file exists, Otherwise False</returns>
   public bool Exists(string path)
   {
     var fileExists = File.Exists(path);
@@ -46,17 +55,38 @@ public class FileServices : IFileServices
     return fileExists;
   }
 
+  /// <summary>
+  /// Reads all of the content for the specified file and returns them in string format
+  /// </summary>
+  /// <param name="path">The path of the file to read from</param>
+  /// <returns>The contents of the file</returns>
   public string ReadText(string path)
   {
-    var fileContents = File.ReadAllText(path);
+    try
+    {
+      var fileContents = File.ReadAllText(path);
 
-    return fileContents ?? string.Empty;
+      return fileContents;
+    }
+    catch (Exception)
+    {
+      // Handle the errors with reading the file in the calling code
+      return string.Empty;
+    }
+  }
+
+  public static class Constants
+  {
+    private const string defaultConfigName = "config.json";
+    public const string defaultFileNameDebugMode = $"Files//{defaultConfigName}";
+    public const string defaultFileNameReleaseMode = defaultConfigName;
   }
 }
 
 public interface IFileServices
 {
   public string GetFullPath(string? fileName = null);
+  public string SetDefaultFileName();
   public bool Exists(string path);
   public string ReadText(string path);
 }
