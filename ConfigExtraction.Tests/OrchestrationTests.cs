907e8465e842047fb54ba8fcff165c88f855d5a1
Matthew Lewis <mlewis@singular.co.za>
2023-12-15 14:04:34 +0200

    Updated the ConfigExtraction.Tests project

---
+++ ConfigExtraction.Tests/OrchestrationTests.cs
@@ -0,0 +1,16 @@
+﻿namespace ConfigExtraction.Tests;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class OrchestrationTests
+{
+  [Fact]
+  public void Test()
+  {
+    // TODO: placeholder, will add some nice tests to test the Orchestration logic
+  }
+}
