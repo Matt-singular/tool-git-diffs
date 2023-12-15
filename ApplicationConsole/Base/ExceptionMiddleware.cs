b368dacf9ebdb4c1822544ad4b3b0c6d66b269f2
Matthew Lewis <mlewis@singular.co.za>
2023-12-15 18:26:07 +0200

    Added custom exception middleware logic

---
+++ ApplicationConsole/Base/ExceptionMiddleware.cs
@@ -0,0 +1,32 @@
+﻿namespace ApplicationConsole.Base;
+
+using System;
+
+/// <summary>
+/// Catch all the thrown and unhandled exceptions to allow custom handling logic
+/// </summary>
+public static class ExceptionMiddleware
+{
+  public static void SetupGlobalExceptionHandler()
+  {
+    AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+    {
+      // Extract the Exception and message
+      var exception = args.ExceptionObject as Exception;
+      var message = exception?.Message;
+
+      // Write the error message
+      Console.ForegroundColor = ConsoleColor.Red;
+      Console.Write(message);
+
+      // Reset the Console colour and keep it open
+      Console.ResetColor();
+#if RELEASE
+      // Only block the console from auto closing on error if we're in Release Mode.
+      // This allows for nice friendly error messages to be displayed when using the published exe
+      // without slowing down the local development process.
+      Console.ReadLine();
+#endif
+    };
+  }
+}
--- ApplicationConsole/Base/Program.cs
+++ ApplicationConsole/Base/Program.cs
@@ -4,20 +4,17 @@ using Microsoft.Extensions.DependencyInjection;
 
 // Application startup - set up DI container
 var serviceProvider = new ServiceCollection()
-    .AddApplicationConsoleServices()
-    .AddConfigExtractionServices()
-    .BuildServiceProvider();
+  .AddApplicationConsoleServices()
+  .AddConfigExtractionServices()
+  .BuildServiceProvider();
+
+// Exception Handling 
+ExceptionMiddleware.SetupGlobalExceptionHandler();
 
 // Call the application orchestration logic
 var applicationOrchestration = serviceProvider.GetRequiredService<ApplicationConsole.IOrchestration>();
 applicationOrchestration.Process();
 
-// Done - ensure the console remains open
+// Ensure the Console remains open
 Console.Write("\nPress Enter to exit...");
 _ = Console.ReadLine();
-
-/* TODO LIST */
-/* 1) Currently don't use DI anywhere, consider adding it
- * 2) XXX
- * 3) XXX
-*/
