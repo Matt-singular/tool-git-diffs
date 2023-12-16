690f742e6b7ad9a0606ed9f98504a87fc8b74ad2
Matthew Lewis <mlewis@singular.co.za>
2023-12-16 21:33:14 +0200

    Updated the ReferenceExtraction orchestration logic

--- ReferenceExtraction/Orchestration.cs
+++ ReferenceExtraction/Orchestration.cs
@@ -6,16 +6,19 @@ using ReferenceExtraction.Services;
 /// <summary>
 /// Orchestration of all the reference extraction related logic
 /// </summary>
-public class Orchestration(IGenerateRegexes generateRegexes) : IOrchestration
+public class Orchestration(IGenerateRegexes generateRegexes, IExtractReferences extractReferences) : IOrchestration
 {
-  public void Process(List<string> commitMessaages, ConfigModel config)
+  public void Process(List<string> commitMessages, ConfigModel config)
   {
     // 1) Extract the Regex patterns from the config
     var regexPatterns = generateRegexes.Process(config);
+
+    // 2) Extract the actual references
+    var references = extractReferences.Process(regexPatterns, commitMessages, config);
   }
 }
 
 public interface IOrchestration
 {
-  public void Process(List<string> commitMessaages, ConfigModel config);
+  public void Process(List<string> commitMessages, ConfigModel config);
 }
