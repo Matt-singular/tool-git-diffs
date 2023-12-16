8e3d5f2b4ee1feec99c36abc00c262564fd9d208
Matthew Lewis <mlewis@singular.co.za>
2023-12-16 12:38:45 +0200

    Added the ReferenceExtraction.Tests project

---
+++ ReferenceExtraction.Tests/Base/GlobalUsings.cs
@@ -0,0 +1,8 @@
+// Testing Framework
+global using Xunit;
+
+// Used to mock objects, services/behaviours to isolate and test individual components of a system independently
+global using NSubstitute;
+
+// Enhance the readability of assertions
+global using FluentAssertions;
