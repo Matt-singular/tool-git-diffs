af7eb9115253514dae3a70bf4bc0ac6ed606f04d
Matthew Lewis <mlewis@singular.co.za>
2023-12-16 15:12:12 +0200

    Added the REferenceExtraction.Tests DI Unit tests

---
+++ ReferenceExtraction.Tests/Base/DependencyInjectionExtensionTests.cs
@@ -0,0 +1,36 @@
+﻿namespace ReferenceExtraction.Tests.Base;
+
+using Microsoft.Extensions.DependencyInjection;
+using ReferenceExtraction.Base;
+using ReferenceExtraction.Services;
+using SharedTestDependencies;
+
+public class DependencyInjectionExtensionTests
+{
+  [Fact]
+  public void AddReferenceExtractionServices_RegistersSingletonServices()
+  {
+    // Arrange
+    var serviceCollection = new ServiceCollection();
+
+    // Act
+    serviceCollection.AddReferenceExtractionServices();
+
+    // Assert
+    serviceCollection.Should().ContainSingle(descriptor => Helpers.ValidDependencyType<IOrchestration, Orchestration>(descriptor, ServiceLifetime.Singleton));
+  }
+
+  [Fact]
+  public void AddReferenceExtractionServices_RegistersScopedServices()
+  {
+    // Arrange
+    var serviceCollection = new ServiceCollection();
+
+    // Act
+    serviceCollection.AddReferenceExtractionServices();
+
+    // Assert
+    serviceCollection.Should().ContainSingle(descriptor => Helpers.ValidDependencyType<IGenerateRegexes, GenerateRegexes>(descriptor, ServiceLifetime.Scoped));
+    serviceCollection.Should().ContainSingle(descriptor => Helpers.ValidDependencyType<IExtractReferences, ExtractReferences>(descriptor, ServiceLifetime.Scoped));
+  }
+}
