aa728a6e30f5ade120a3515c213880688a586b51
Matthew Lewis <mlewis@singular.co.za>
2024-05-31 15:09:32 +0200

    Added integration tests to pull commits

---
+++ Integration.Tests/ExtractReferences/Commits/PullCommitsExtractServiceIntegrationTests.cs
@@ -0,0 +1,42 @@
+﻿namespace Integration.Tests.ExtractReferences.Commits;
+
+using FluentAssertions;
+using global::ExtractReferences.Authorisation;
+using global::ExtractReferences.Commits;
+using NSubstitute;
+using TestHelpers;
+
+public class PullCommitsExtractServiceIntegrationTests
+{
+  [Fact]
+  public async Task ExtractReferences_PullCommitsExtractService_InvalidReference_ShouldThrowError()
+  {
+    // Arrange
+    var mockedSecretSettings = ConfigHelpers.MockSecretSettings();
+    var gitHubAuthService = Substitute.ForPartsOf<GitHubAuthExtractService>(mockedSecretSettings);
+    var pullCommitsExtractService = Substitute.ForPartsOf<PullCommitsExtractService>(mockedSecretSettings, gitHubAuthService);
+
+    // Act
+    var repositoryName = mockedSecretSettings.Value.GitHubRepositories.First();
+    Func<Task> act = async () => await pullCommitsExtractService.ProcessAsync(repositoryName, "unknown", "12.2.1");
+
+    // Assert
+    await act.Should().ThrowAsync<Octokit.NotFoundException>();
+  }
+
+  [Fact]
+  public async Task ExtractReferences_PullCommitsExtractService_ValidReferences()
+  {
+    // Arrange
+    var mockedSecretSettings = ConfigHelpers.MockSecretSettings();
+    var gitHubAuthService = Substitute.ForPartsOf<GitHubAuthExtractService>(mockedSecretSettings);
+    var pullCommitsExtractService = Substitute.ForPartsOf<PullCommitsExtractService>(mockedSecretSettings, gitHubAuthService);
+
+    // Act
+    var repositoryName = mockedSecretSettings.Value.GitHubRepositories.First();
+    var result = await pullCommitsExtractService.ProcessAsync(repositoryName, "12.2.0", "12.2.1");
+
+    // Assert
+    result.CommitDetails.Should().NotBeEmpty();
+  }
+}
