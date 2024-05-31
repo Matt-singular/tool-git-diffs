a8b26f8aa6c594ee5b6aa941f79a4ca06728c1f2
Matthew Lewis <mlewis@singular.co.za>
2024-05-31 15:08:25 +0200

    Added the Octokit extensions file

---
+++ ExtractReferences/Extensions/OctokitExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace ExtractReferences.Extensions;
+using Octokit;
+
+public static class OctokitExtensions
+{
+  /// <summary>
+  /// Checks if the particular commit was a merge or not
+  /// </summary>
+  /// <param name="commit">The Octokit GitHubCommit response</param>
+  /// <returns>True if the commit in question was a merge commit, Otherwise False</returns>
+  public static bool CheckMergeCommit(this GitHubCommit commit)
+  {
+    var committer = commit.Commit.Committer.Name;
+    var gitHubCommitter = "GitHub";
+    var isMergeCommit = committer.Equals(gitHubCommitter);
+
+    return isMergeCommit;
+  }
+}
