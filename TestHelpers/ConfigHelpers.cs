8aaacf15f6984d7d43294cd957ffd58cbe86a5de
Matthew Lewis <mlewis@singular.co.za>
2024-05-31 15:04:07 +0200

    Config test helpers

---
+++ TestHelpers/ConfigHelpers.cs
@@ -0,0 +1,25 @@
+﻿namespace TestHelpers;
+
+using Configuration.ConfigurationHelpers;
+using Configuration.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+public static class ConfigHelpers
+{
+  public static IOptions<SecretSettings> MockSecretSettings()
+  {
+    // Pull values from config.json
+    var config = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile(ConfigurationHelpers.GetConfigJsonPath())
+        .AddUserSecrets<Program>()
+        .Build();
+
+    // Mocked SecretSettings
+    var secretSettings = config.GetSection("Secrets").Get<SecretSettings>();
+    var mockedSecretSettings = Options.Create(secretSettings);
+
+    return mockedSecretSettings;
+  }
+}
