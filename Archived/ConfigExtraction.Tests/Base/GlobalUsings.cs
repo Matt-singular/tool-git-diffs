66eabb5d7f99955db2ef7d6cb77f34dd6a7fb0cd
Matthew Lewis <mlewis@singular.co.za>
2024-06-19 19:16:58 +0200

    Remove archived version

--- Archived/ConfigExtraction.Tests/Base/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-// Testing Framework
-global using Xunit;
-
-// Used to mock objects, services/behaviours to isolate and test individual components of a system independently
-global using NSubstitute;
-
-// Enhance the readability of assertions
-global using FluentAssertions;
