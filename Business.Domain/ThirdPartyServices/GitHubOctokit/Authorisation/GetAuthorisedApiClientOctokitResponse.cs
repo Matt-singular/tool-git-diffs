aba64262671b656b634f9b38249bce1a354f0211
Matthew Lewis <mlewis@singular.co.za>
2024-06-21 23:12:35 +0200

    Enhanced Octokit AuthorisedApiClient Response

--- Business.Domain/ThirdPartyServices/GitHubOctokit/Authorisation/GetAuthorisedApiClientOctokitResponse.cs
+++ Business.Domain/ThirdPartyServices/GitHubOctokit/Authorisation/GetAuthorisedApiClientOctokitResponse.cs
@@ -2,10 +2,18 @@
 
 using Octokit;
 
+/// <summary>
+/// The authorised GitHub Octokit API client
+/// </summary>
 public class GetAuthorisedApiClientOctokitResponse : GitHubClient
 {
   public GetAuthorisedApiClientOctokitResponse(string accessToken) : base(new ProductHeaderValue("tool-git-diffs"))
   {
+    if (string.IsNullOrEmpty(accessToken))
+    {
+      throw new ArgumentNullException(nameof(accessToken), "GitHub access token has not been configured");
+    }
+
     this.Credentials = new Credentials(accessToken);
   }
 }
