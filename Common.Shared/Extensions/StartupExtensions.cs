b975df97d3ab0c3d527e7510d8b53bc5e7b09a58
Matthew Lewis <mlewis@singular.co.za>
2024-06-22 21:52:44 +0200

    Scaffolded out some startup extensions in shared project

---
+++ Common.Shared/Extensions/StartupExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace Common.Shared.Extensions;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+public static class StartupExtensions
+{
+  /// <summary>
+  /// Sets up the application configuration.
+  /// </summary>
+  /// <returns>The host builder with the configured application settings.</returns>
+  public static IHostBuilder SetupCommonSharedConfiguration<TStartupClass>(this IHostBuilder builder)
+    where TStartupClass : class
+  {
+    // Add config.json file
+    builder.ConfigureAppConfiguration((context, config) => config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true));
+
+    // Enable User Secrets
+    builder.ConfigureAppConfiguration((context, config) => config.AddUserSecrets<TStartupClass>());
+
+    // Configure config.json settings
+    builder.ConfigureServices((context, services) =>
+    {
+      // Configure strongly-typed settings objects
+      //services.Configure<SecretSettings>(context.Configuration.GetSection("Secrets"));
+      //services.Configure<FileSettings>(context.Configuration.GetSection("Files"));
+      //services.Configure<CommitSettings>(context.Configuration.GetSection("Commits"));
+    });
+
+    return builder;
+  }
+}
