37ae8423e4d36f19f02900dc7108627654ba854c
Matthew Lewis <mlewis@singular.co.za>
2024-06-22 21:52:30 +0200

    Configured user secrets and config

---
+++ Common.Shared/Config/SecretSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Common.Shared.Config;
+
+/// <summary>
+/// Represents the secrets settings for the application as found in config.json
+/// </summary>
+public class SecretSettings
+{
+  public required string GitHubAccessToken { get; set; }
+  public required string GitHubOrganisationName { get; set; }
+  public required string[] GitHubRepositories { get; set; }
+}
