e00cf599d336994e271028ede6febbbbb9125423
Matthew Lewis <mlewis@singular.co.za>
2024-06-22 21:53:33 +0200

    Added mini service to retrieve an authorised Octokit API client

--- Business.Domain/ThirdPartyServices/GitHubOctokit/Authorisation/IGetAuthorisedApiClientOctokitService.cs
+++ Business.Domain/ThirdPartyServices/GitHubOctokit/Authorisation/IGetAuthorisedApiClientOctokitService.cs
@@ -1,6 +1,9 @@
 ﻿namespace Business.Domain.ThirdPartyServices.GitHubOctokit.Authorisation;
 
+/// <summary>
+/// A service to get an authorised API client for the GitHub Octokit API.
+/// </summary>
 public interface IGetAuthorisedApiClientOctokitService
 {
-
+  public GetAuthorisedApiClientOctokitResponse Process();
 }
---
+++ Business.Infrastructure/ThirdPartyServices/GitHubOctokit/Authorisation/GetAuthorisedApiClientOctokitService.cs
@@ -0,0 +1,20 @@
+﻿namespace Business.Infrastructure.ThirdPartyServices.GitHubOctokit.Authorisation;
+
+using Business.Domain.ThirdPartyServices.GitHubOctokit.Authorisation;
+using Common.Shared.Config;
+using Microsoft.Extensions.Options;
+
+/// <summary>
+/// Fetches the GitHub access token from the secret settings and creates an authorised Octokit API client.
+/// </summary>
+/// <param name="secretSettings">Contains the configured GitHub access token which determines the capability of the Octokit API</param>
+public class GetAuthorisedApiClientOctokitService(IOptions<SecretSettings> secretSettings) : IGetAuthorisedApiClientOctokitService
+{
+  public GetAuthorisedApiClientOctokitResponse Process()
+  {
+    var accessToken = secretSettings.Value.GitHubAccessToken;
+    var octokitApiClient = new GetAuthorisedApiClientOctokitResponse(accessToken);
+
+    return octokitApiClient;
+  }
+}
