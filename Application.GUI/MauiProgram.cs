69c418b96c35997f62eb02ceb96f1abd933b3ed6
Matthew Lewis <mlewis@singular.co.za>
2024-06-24 21:43:09 +0200

    Added configuration and DI startup logic

--- Application.GUI/MauiProgram.cs
+++ Application.GUI/MauiProgram.cs
@@ -1,5 +1,7 @@
 ﻿namespace Application.GUI;
 
+using Common.Shared;
+using Common.Shared.Extensions;
 using Microsoft.Extensions.Logging;
 
 public static class MauiProgram
@@ -7,6 +9,20 @@ public static class MauiProgram
   public static MauiApp CreateMauiApp()
   {
     var builder = MauiApp.CreateBuilder();
+
+    // Configurations
+    builder.Configuration.SetupCommonSharedConfiguration();
+    builder.Services.SetupCommonSharedConfigSettings(builder.Configuration);
+
+    // Dependency Injection
+    builder.Services.AddCommonSharedServices();
+
+    // TODO: testing things
+    var serviceProvider = builder.Services.BuildServiceProvider();
+    var testService = serviceProvider.GetService<ITestService>();
+    testService.Process();
+
+    // Maui App
     builder
       .UseMauiApp<App>()
       .ConfigureFonts(fonts =>
@@ -15,6 +31,7 @@ public static class MauiProgram
         fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
       });
 
+    // Logging
 #if DEBUG
     builder.Logging.AddDebug();
 #endif
