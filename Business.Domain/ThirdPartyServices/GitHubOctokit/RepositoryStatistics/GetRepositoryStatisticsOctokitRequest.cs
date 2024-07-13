96dab426402cb8f0224947a9df569a4fca5e43c4
Matthew Lewis <mlewis@singular.co.za>
2024-07-13 21:52:36 +0200

    Renamed the author statistic logic:

---
+++ Business.Domain/ThirdPartyServices/GitHubOctokit/RepositoryStatistics/GetRepositoryStatisticsOctokitRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace Business.Domain.ThirdPartyServices.GitHubOctokit.RepositoryStatistics;
+
+public class GetRepositoryStatisticsOctokitRequest
+{
+  public required string RepositoryName { get; set; }
+
+  /// <summary>
+  /// Where the repository statistics should be pulled from.
+  /// This will be either a branch or a tag.
+  /// </summary>
+  public required string FromBranchOrTag { get; set; }
+
+  /// <summary>
+  /// Where the repository statistics should be pulled until.
+  /// This will be either a branch or a tag.
+  /// </summary>
+  public required string ToBranchOrTag { get; set; }
+}
