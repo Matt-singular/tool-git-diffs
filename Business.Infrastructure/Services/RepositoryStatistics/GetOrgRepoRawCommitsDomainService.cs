9ed8f2839a11e5059ed2316dffd988ed72d21d99
Matthew Lewis <mlewis@singular.co.za>
2024-07-25 21:04:14 +0200

    Maps the Octokit repository commits

--- Business.Infrastructure/Services/RepositoryStatistics/GetOrgRepoRawCommitsDomainService.cs
+++ Business.Infrastructure/Services/RepositoryStatistics/GetOrgRepoRawCommitsDomainService.cs
@@ -10,12 +10,16 @@ using Business.Domain.ThirdPartyServices.GitHubOctokit.RepositoryStatistics;
 /// <param name="getOrgRepoCommitsOctokitService">Gets the list of commits for the specified organisation repository using the Octokit API</param>
 public class GetOrgRepoRawCommitsDomainService(IGetOrgRepoCommitsOctokitService getOrgRepoCommitsOctokitService) : IGetOrgRepoRawCommitsDomainService
 {
-  public Task<GetOrgRepoRawCommitsDomainResponse> GetRawCommits(GetOrgRepoRawCommitsDomainRequest domainRequest)
+  public async Task<GetOrgRepoRawCommitsDomainResponse> GetRawCommits(GetOrgRepoRawCommitsDomainRequest domainRequest)
   {
-    throw new NotImplementedException();
+    var getRepositoryStatisticsOctokitResponses = await GetRepositoryCommitsFromOctokitAsync(domainRequest).ConfigureAwait(false);
+
+    var domainResponse = MapRepositoryCommits(getRepositoryStatisticsOctokitResponses);
+
+    return domainResponse;
   }
 
-  public async Task<List<GetOrgRepoCommitsOctokitDomainResponse>> GetRepositoryStatisticsFromOctokitAsync(GetOrgRepoRawCommitsDomainRequest domainRequest)
+  public async Task<List<GetOrgRepoCommitsOctokitDomainResponse>> GetRepositoryCommitsFromOctokitAsync(GetOrgRepoRawCommitsDomainRequest domainRequest)
   {
     var getOrgRepoCommitsOctokitTasks = domainRequest.Repositories.Select(repo =>
     {
@@ -37,5 +41,23 @@ public class GetOrgRepoRawCommitsDomainService(IGetOrgRepoCommitsOctokitService 
     // Wait for all of the Octokit API Tasks to complete
     var getOrgRepoCommitsOctokitResponses = await Task.WhenAll(getOrgRepoCommitsOctokitTasks).ConfigureAwait(false);
     return getOrgRepoCommitsOctokitResponses.ToList();
+  }
+
+  public static GetOrgRepoRawCommitsDomainResponse MapRepositoryCommits(List<GetOrgRepoCommitsOctokitDomainResponse> getRepositoryStatisticsOctokitResponses)
+  {
+    var repositoryCommits = getRepositoryStatisticsOctokitResponses
+      .SelectMany(repo =>
+      {
+        var repoName = repo.RepositoryName;
+        return repo.Commits.Select(repoCommits =>
+        {
+          var repositoryCommit = (GetOrgRepoRawCommitsDomainResponse.RepositoryCommits)repoCommits;
+          repositoryCommit.RepositoryName = repoName;
+          return repositoryCommit;
+        });
+      });
+
+    var domainResponse = (GetOrgRepoRawCommitsDomainResponse)repositoryCommits.ToList();
+    return domainResponse;
   }
 }
