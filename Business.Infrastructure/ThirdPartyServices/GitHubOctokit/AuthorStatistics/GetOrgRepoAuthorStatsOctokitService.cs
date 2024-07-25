d47327fc9679b783ad93e8c5fe5af6ab61704ae0
Matthew Lewis <mlewis@singular.co.za>
2024-07-25 18:01:59 +0200

    Updated the org repo author stats Octokit service

--- Business.Infrastructure/ThirdPartyServices/GitHubOctokit/AuthorStatistics/GetOrgRepoAuthorStatsOctokitService.cs
+++ Business.Infrastructure/ThirdPartyServices/GitHubOctokit/AuthorStatistics/GetOrgRepoAuthorStatsOctokitService.cs
@@ -5,12 +5,17 @@ using Business.Domain.ThirdPartyServices.GitHubOctokit.Statistics;
 using Common.Shared.Config;
 using Microsoft.Extensions.Options;
 
-public class GetAuthorRepoStatsOctokitService(IOptions<SecretSettings> secretSettings, IGetAuthorisedApiClientOctokitService getAuthorisedApiClientOctokitService) : IGetAuthorRepoStatsOctokitService
+/// <summary>
+/// Gets some basic statistics for the specified author in the specified repository
+/// </summary>
+/// <param name="secretSettings">Contains the Organisation's name to run use when pulling repository statistics</param>
+/// <param name="getAuthorisedApiClientOctokitService">The service that encapsulates the Octokit API client</param>
+public class GetOrgRepoAuthorStatsOctokitService(IOptions<SecretSettings> secretSettings, IGetAuthorisedApiClientOctokitService getAuthorisedApiClientOctokitService) : IGetOrgRepoAuthorStatsOctokitService
 {
   private readonly SecretSettings secretSettings = secretSettings.Value;
   private readonly GetAuthorisedApiClientOctokitResponse octokitApiClient = getAuthorisedApiClientOctokitService.Process();
 
-  public async Task<GetAuthorRepoStatsOctokitDomainResponse> ProcessAsync(GetAuthorRepoStatsOctokitDomainRequest request)
+  public async Task<GetOrgRepoAuthorStatsOctokitDomainResponse> ProcessAsync(GetOrgRepoAuthorStatsOctokitDomainRequest request)
   {
     // Secrets
     var organisation = secretSettings.GitHubOrganisationName;
@@ -30,7 +35,7 @@ public class GetAuthorRepoStatsOctokitService(IOptions<SecretSettings> secretSet
       .Select(authorStats => (authorName: authorStats.Key, commitCount: authorStats.Count()))
       .ToList();
 
-    return new GetAuthorRepoStatsOctokitDomainResponse
+    return new GetOrgRepoAuthorStatsOctokitDomainResponse
     {
       RepositoryName = repositoryDetail.Name,
       AuthorStatistics = authorStats
