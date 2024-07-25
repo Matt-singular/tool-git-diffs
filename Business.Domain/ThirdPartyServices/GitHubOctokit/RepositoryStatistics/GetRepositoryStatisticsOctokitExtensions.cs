85e511233e2e1466cf957841c3d403eb7e8674c0
Matthew Lewis <mlewis@singular.co.za>
2024-07-25 19:59:29 +0200

    Updated the CheckMergeCommit Octokit extension method logic to identify more merge commits

--- Business.Domain/ThirdPartyServices/GitHubOctokit/RepositoryStatistics/GetRepositoryStatisticsOctokitExtensions.cs
+++ Business.Domain/ThirdPartyServices/GitHubOctokit/RepositoryStatistics/GetRepositoryStatisticsOctokitExtensions.cs
@@ -1,7 +1,12 @@
 ﻿namespace Business.Domain.ThirdPartyServices.GitHubOctokit.RepositoryStatistics;
 
-public static class GetRepositoryStatisticsOctokitExtensions
+using System.Text.RegularExpressions;
+
+public static partial class GetRepositoryStatisticsOctokitExtensions
 {
+  [GeneratedRegex(@"^(?:Merge branch\s+)?'([^']+)' of (https:\/\/)?github\.com(\/)?(:)?[^\/]+\/[^\/]+ into ([^\s]+)$")]
+  private static partial Regex CommitMergeMessageRegex();
+
   /// <summary>
   /// Checks if the particular commit was a merge or not
   /// </summary>
@@ -9,10 +14,21 @@ public static class GetRepositoryStatisticsOctokitExtensions
   /// <returns>True if the commit in question was a merge commit, Otherwise False</returns>
   public static bool CheckMergeCommit(this Octokit.GitHubCommit commit)
   {
+    // Merge Commit was done via GitHub
     var committerName = commit.Commit.Committer.Name;
     var gitHubCommitter = "GitHub";
 
-    var isMergeCommit = committerName.Equals(gitHubCommitter);
-    return isMergeCommit;
+    var isGitHubMergeCommit = committerName.Equals(gitHubCommitter);
+    if (isGitHubMergeCommit)
+    {
+      // We already know it's a merge commit, so we short-circuit
+      return true;
+    }
+
+    // Merge Commit was done explicitly by a particular user
+    var commitMessage = commit.Commit.Message.Trim();
+    var isMergeCommitMessage = CommitMergeMessageRegex().IsMatch(commitMessage);
+
+    return isMergeCommitMessage;
   }
 }
