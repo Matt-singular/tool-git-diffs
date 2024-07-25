3816d406e92a942d82b7b4250a71e6d95a41367e
Matthew Lewis <mlewis@singular.co.za>
2024-07-25 20:05:11 +0200

    Filter out merge commits if specified in the domain request

--- Business.Infrastructure/ThirdPartyServices/GitHubOctokit/RepositoryStatistics/GetOrgRepoCommitsOctokitService.cs
+++ Business.Infrastructure/ThirdPartyServices/GitHubOctokit/RepositoryStatistics/GetOrgRepoCommitsOctokitService.cs
@@ -11,28 +11,28 @@ using Microsoft.Extensions.Options;
 /// </summary>
 /// <param name="secretSettings">Contains the Organisation's name to run use when pulling repository statistics</param>
 /// <param name="getAuthorisedApiClientOctokitService">The service that encapsulates the Octokit API client</param>
-public class GetOrgRepoCommitsOctokitService(IOptions<SecretSettings> secretSettings, IGetAuthorisedApiClientOctokitService getAuthorisedApiClientOctokitService) : IGetRepositoryStatisticsOctokitService
+public class GetOrgRepoCommitsOctokitService(IOptions<SecretSettings> secretSettings, IGetAuthorisedApiClientOctokitService getAuthorisedApiClientOctokitService) : IGetOrgRepoCommitsOctokitService
 {
   private readonly SecretSettings secretSettings = secretSettings.Value;
   private readonly GetAuthorisedApiClientOctokitResponse octokitApiClient = getAuthorisedApiClientOctokitService.CreateClient();
 
-  public async Task<GetRepositoryStatisticsOctokitDomainResponse> ProcessAsync(GetRepositoryStatisticsOctokitDomainRequest request)
+  public async Task<GetOrgRepoCommitsOctokitDomainResponse> ProcessAsync(GetOrgRepoCommitsOctokitDomainRequest request)
   {
     // Gets the repository statistics from Octokit
     var getRepoStatsOctokitTask = GetRepositoryStatisticsFromOctokit(request, secretSettings.GitHubOrganisationName);
     var repoStatsOctokitResponse = await getRepoStatsOctokitTask.ConfigureAwait(false);
 
     // Maps the Octokit response to the domain response
-    var repositoryStatisticsResponse = new GetRepositoryStatisticsOctokitDomainResponse
+    var repositoryStatisticsResponse = new GetOrgRepoCommitsOctokitDomainResponse
     {
       RepositoryName = request.RepositoryName,
-      Commits = MapRepositoryCommitStatistics(repoStatsOctokitResponse)
+      Commits = MapRepositoryCommitStatistics(repoStatsOctokitResponse, request.ExcludeMergeCommits)
     };
 
     return repositoryStatisticsResponse;
   }
 
-  public async Task<Octokit.CompareResult> GetRepositoryStatisticsFromOctokit(GetRepositoryStatisticsOctokitDomainRequest request, string? organisationName)
+  public async Task<Octokit.CompareResult> GetRepositoryStatisticsFromOctokit(GetOrgRepoCommitsOctokitDomainRequest request, string? organisationName)
   {
     // Gets the repository statistics from Octokit
     var getRepositoryCommitsOctokitTask = this.octokitApiClient.GetRepositoryCommitsAsync(repositoryOwner: organisationName, request.RepositoryName, request.FromBranchOrTag, request.ToBranchOrTag);
@@ -41,9 +41,10 @@ public class GetOrgRepoCommitsOctokitService(IOptions<SecretSettings> secretSett
     return getRepositoryCommitsOctokitResponse;
   }
 
-  private static List<GetRepositoryStatisticsOctokitDomainResponse.CommitDetails> MapRepositoryCommitStatistics(Octokit.CompareResult repoStatsOctokitResponse)
+  private static List<GetOrgRepoCommitsOctokitDomainResponse.CommitDetails> MapRepositoryCommitStatistics(Octokit.CompareResult repoStatsOctokitResponse, bool excludeMergeCommits)
   {
-    var commits = repoStatsOctokitResponse.Commits.Select(commit => new GetRepositoryStatisticsOctokitDomainResponse.CommitDetails
+    // Maps the Octokit commits into a custom domain response
+    var commits = repoStatsOctokitResponse.Commits.Select(commit => new GetOrgRepoCommitsOctokitDomainResponse.CommitDetails
     {
       Hash = commit.Sha,
       AuthorName = commit.Author.Login,
@@ -52,6 +53,12 @@ public class GetOrgRepoCommitsOctokitService(IOptions<SecretSettings> secretSett
       DateOfCommit = commit.Commit.Author.Date.Date,
       IsMergeCommit = commit.CheckMergeCommit()
     });
+
+    if (excludeMergeCommits)
+    {
+      // Filter out all the merge commits
+      commits = commits.Where(commit => !commit.IsMergeCommit);
+    }
 
     return commits.ToList();
   }
