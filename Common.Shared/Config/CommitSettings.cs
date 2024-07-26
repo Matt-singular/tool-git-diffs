b285ed4d6353010c34ea7876e35a2c2daf702d97
Matthew Lewis <mlewis@singular.co.za>
2024-07-26 16:26:38 +0200

    Added Commit configuration settings and settings model

---
+++ Common.Shared/Config/CommitSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace Common.Shared.Config;
+
+/// <summary>
+/// Represents the commit settings for the application as found in the appsettings.json file
+/// </summary>
+public class CommitSettings
+{
+  public required List<CommitPattern> Patterns { get; set; }
+  public required List<string> SortOrder { get; set; }
+  public required string OrderBy { get; set; }
+
+  public class CommitPattern
+  {
+    public required string Header { get; set; }
+    public required string Pattern { get; set; }
+    public bool? GroupBy { get; set; }
+    public int? Priority { get; set; }
+  }
+}
