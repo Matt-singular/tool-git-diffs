7bb9b4aa8a1587af03f199cff92114b0d5a67a00
Matthew Lewis <mlewis@singular.co.za>
2024-10-13 21:13:15 +0200

    Added placeholder logic to test the excel sheet export functionality

--- Application.GUI/MainPage.xaml.cs
+++ Application.GUI/MainPage.xaml.cs
@@ -1,22 +1,27 @@
 ﻿namespace Application.GUI;
 
+using Business.Domain.Services.Excel;
 using Business.Domain.ThirdPartyServices.GitHubOctokit.RepositoryStatistics;
 
 public partial class MainPage : ContentPage
 {
   private string FromReference { get => FromReferenceEntryElement.Text; }
   private string ToReference { get => ToReferenceEntryElement.Text; }
-  private readonly IGetOrgRepoCommitsOctokitService getOrgRepoCommitsOctokitService;
 
-  public MainPage(IGetOrgRepoCommitsOctokitService getOrgRepoCommitsOctokitService)
+  private readonly IGetOrgRepoCommitsOctokitService getOrgRepoCommitsOctokitService;
+  private readonly ICreateExcelSheetDomainService createExcelSheetDomainService;
+
+  public MainPage(IGetOrgRepoCommitsOctokitService getOrgRepoCommitsOctokitService, ICreateExcelSheetDomainService createExcelSheetDomainService)
   {
     this.getOrgRepoCommitsOctokitService = getOrgRepoCommitsOctokitService;
+    this.createExcelSheetDomainService = createExcelSheetDomainService;
 
     InitializeComponent();
   }
 
   private void OnGenerateClicked(object sender, EventArgs e)
   {
+    // POC TEST 1
     var domainRequest = new GetOrgRepoCommitsOctokitDomainRequest
     {
       RepositoryName = "placeholder",
@@ -25,6 +30,9 @@ public partial class MainPage : ContentPage
       ExcludeMergeCommits = false
     };
 
-    var domainResponse = getOrgRepoCommitsOctokitService.ProcessAsync(domainRequest).GetAwaiter().GetResult();
+    //var domainResponse = getOrgRepoCommitsOctokitService.ProcessAsync(domainRequest).GetAwaiter().GetResult();
+
+    // POC TEST 2
+    createExcelSheetDomainService.Process();
   }
 }
