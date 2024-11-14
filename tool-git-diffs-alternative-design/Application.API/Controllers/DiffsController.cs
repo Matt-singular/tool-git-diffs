f8528612c93db9086e411958c4696ed12ab12e6c
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2024-11-14 15:26:13 +0200

    Added the get-raw-commits Diffs placeholder endpoint

---
+++ tool-git-diffs-alternative-design/Application.API/Controllers/DiffsController.cs
@@ -0,0 +1,24 @@
+﻿namespace Application.API.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+/// <summary>
+/// Endpoints to pull diff information for specific repositories and differences
+/// </summary>
+[ApiController]
+[Route("api/[controller]")]
+public class DiffsController : Controller
+{
+  /// <summary>
+  /// Gets the raw commits for the repository
+  /// </summary>
+  /// <returns>The raw unprocessed commits for the repository</returns>
+  [HttpGet("get-raw-commits")]
+  public object GetRawCommits()
+  {
+    return new
+    {
+      Message = "Hello World"
+    };
+  }
+}
