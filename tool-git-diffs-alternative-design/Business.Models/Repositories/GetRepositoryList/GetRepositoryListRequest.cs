4dbcb2511288e09d214ffbb6d76bdfaa73c0a06e
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2024-11-15 11:30:09 +0200

    Implemented the GetRepositoryListRequest including model validation functionality

--- tool-git-diffs-alternative-design/Business.Models/Repositories/GetRepositoryList/GetRepositoryListRequest.cs
+++ tool-git-diffs-alternative-design/Business.Models/Repositories/GetRepositoryList/GetRepositoryListRequest.cs
@@ -1,9 +1,28 @@
 ﻿namespace Business.Models.Repositories.GetRepositoryList;
 
+using Common.Shared.Models.Exceptions;
+
 /// <summary>
 /// Contains the data needed to get the list of repositories
 /// </summary>
 public class GetRepositoryListRequest
 {
-    public GetRepositoryListRequest() => throw new NotImplementedException();
+  public string? OrganisationName { get; set; }
+  public string? UserName { get; set; }
+
+  /// <summary>
+  /// Determines whether the model is valid
+  /// </summary>
+  /// <returns>True if the model is valid, otherwise False</returns>
+  public bool ValidateModel()
+  {
+    var notValid = this.OrganisationName is not null && this.UserName is not null;
+
+    if (notValid)
+    {
+      throw new BadRequestException($"Bad Request for {nameof(GetRepositoryListRequest)}");
+    }
+
+    return true;
+  }
 }
