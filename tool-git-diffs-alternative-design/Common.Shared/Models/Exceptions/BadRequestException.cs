ebab27441aed9c8600c22d8bbd31063ce6baab76
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2024-11-15 11:29:07 +0200

    Added a custom BadRequestException

---
+++ tool-git-diffs-alternative-design/Common.Shared/Models/Exceptions/BadRequestException.cs
@@ -0,0 +1,21 @@
+﻿namespace Common.Shared.Models.Exceptions;
+
+using System;
+
+/// <summary>
+/// Represents a bad request that has failed validations (error code 400)
+/// </summary>
+public class BadRequestException : Exception
+{
+  public BadRequestException() : base("The request is invalid.")
+  {
+  }
+
+  public BadRequestException(string message) : base(message)
+  {
+  }
+
+  public BadRequestException(string message, Exception innerException) : base(message, innerException)
+  {
+  }
+}
