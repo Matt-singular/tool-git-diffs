5c707a13f77390185d91df1498c00f4c838c3386
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2024-11-15 11:31:12 +0200

    Added a RepositoryDetal shared model

---
+++ tool-git-diffs-alternative-design/Common.Shared/Models/Repositories/RepositoryDetail.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.Shared.Models.Repositories;
+
+/// <summary>
+/// Contains details about a repository
+/// </summary>
+public class RepositoryDetail
+{
+  public long RepositoryId { get; set; }
+  public string RepositoryName { get; set; } = null!;
+}
