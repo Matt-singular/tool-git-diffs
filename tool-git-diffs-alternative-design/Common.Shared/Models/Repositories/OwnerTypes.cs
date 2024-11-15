dcf57d0064293827377c1b0422aab790c94ee1b2
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2024-11-15 11:30:45 +0200

    Added an OwnerTypes enum for repositories

---
+++ tool-git-diffs-alternative-design/Common.Shared/Models/Repositories/OwnerTypes.cs
@@ -0,0 +1,17 @@
+﻿namespace Common.Shared.Models.Repositories;
+
+/// <summary>
+/// The different types of repository owners
+/// </summary>
+public enum OwnerTypes
+{
+  /// <summary>
+  /// The owner is a GitHub organisation
+  /// </summary>
+  Organisation,
+
+  /// <summary>
+  /// The owner is a GitHub user (individual)
+  /// </summary>
+  User
+}
