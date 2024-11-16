8669e123de975119212f66224201d7f12b34e2b1
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2024-11-16 21:14:32 +0200

    Cleared the commmit settings for now (not yet implemented)

--- tool-git-diffs-alternative-design/Common.Shared/Config/CommitSettings.cs
+++ tool-git-diffs-alternative-design/Common.Shared/Config/CommitSettings.cs
@@ -5,15 +5,4 @@
 /// </summary>
 public class CommitSettings
 {
-  public required List<CommitPattern> Patterns { get; set; }
-  public required List<string> SortOrder { get; set; }
-  public required string OrderBy { get; set; }
-
-  public class CommitPattern
-  {
-    public required string Header { get; set; }
-    public required string Pattern { get; set; }
-    public bool? GroupBy { get; set; }
-    public int? Priority { get; set; }
-  }
 }
