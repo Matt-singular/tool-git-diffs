7b328cbe04ade164fec66c43175726a718dbb556
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2024-11-16 21:13:18 +0200

    Added mocked configurations

---
+++ tool-git-diffs-alternative-design/Common.Shared.Tests/Config/MockedCommitSettings.cs
@@ -0,0 +1,21 @@
+﻿namespace Common.Shared.Tests.Config;
+
+using Common.Shared.Config;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+
+/// <summary>
+/// A mocked instance of <see cref="CommitSettings"/>
+/// </summary>
+public static class MockedCommitSettings
+{
+  /// <summary>
+  /// Creates a mocked instance of <see cref="IOptions{TOptions}"/> for <see cref="CommitSettings"/>
+  /// </summary>
+  /// <returns>The mocked instance</returns>
+  public static IOptions<CommitSettings> CreateOptions()
+  {
+    var mockOptions = Substitute.For<IOptions<CommitSettings>>();
+    return mockOptions;
+  }
+}
