1c071a7081e85c9862aeb69b5acf872d35480d83
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2024-11-25 23:10:56 +0200

    Added an Integration test for GetRawCommits domain service

---
+++ tool-git-diffs-alternative-design/IntegrationTests/Common.Shared/Commits/GetRawCommitsIntegrationTests.cs
@@ -0,0 +1,25 @@
+﻿namespace IntegrationTests.Common.Shared.Commits;
+
+using FluentAssertions;
+using global::Common.Shared.Services.Commits.GetRawCommits;
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Integration tests for <see cref="GetRawCommitsDomainService"/>
+/// </summary>
+public class GetRawCommitsIntegrationTests
+{
+  [Fact]
+  public async Task ProcessAsync_ShouldReturnRawCommits()
+  {
+    // Arrange
+    var serviceProvider = IntegrationTestHelpers.RunStartupExtensions().BuildServiceProvider();
+    var service = serviceProvider.GetRequiredService<IGetRawCommitsDomainService>();
+
+    // Act
+    var result = await service.ProcessAsync(740670625, fromReference: "POC", toReference: "main");
+
+    // Assert
+    result.Should().NotBeNull();
+  }
+}
