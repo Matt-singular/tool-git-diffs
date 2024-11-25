4f4fddccd4f3915008d167cab85fe51d3e66bfc7
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2024-11-25 23:10:35 +0200

    Added IntegrationTestHelpers to configure the application's service collection

---
+++ tool-git-diffs-alternative-design/IntegrationTests/IntegrationTestHelpers.cs
@@ -0,0 +1,28 @@
+﻿namespace IntegrationTests;
+
+using global::Business.Domain;
+using global::Common.Shared.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Some helper methods for integration tests
+/// </summary>
+public static class IntegrationTestHelpers
+{
+  /// <summary>
+  /// Run the different project Startup extensions to register configuration & services
+  /// </summary>
+  /// <returns>The configured service collection</returns>
+  public static IServiceCollection RunStartupExtensions()
+  {
+    var configuration = new ConfigurationBuilder().AddCommonSharedConfiguration().Build();
+
+    var serviceCollection = new ServiceCollection()
+      .ConfigureCommonSettings(configuration)
+      .AddCommonSharedServices()
+      .AddBusinessDomainServices();
+
+    return serviceCollection;
+  }
+}
