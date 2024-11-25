b20983c92a21a9281cf8e30b766563efde72dd01
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2024-11-25 21:09:34 +0200

    Added the GetRawCommits domain service

---
+++ tool-git-diffs-alternative-design/Common.Shared/Services/Commits/GetRawCommits/GetRawCommitsDomainService.cs
@@ -0,0 +1,50 @@
+﻿namespace Common.Shared.Services.Commits.GetRawCommits;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Common.Shared.Config;
+using Common.Shared.Extensions.ThirdParty;
+using Common.Shared.Models.Commits;
+using Common.Shared.Models.ThirdParty;
+using Microsoft.Extensions.Options;
+
+/// <inheritdoc cref="IGetRawCommitsDomainService"/>
+public class GetRawCommitsDomainService(IOptions<SecretSettings> secretSettings) : IGetRawCommitsDomainService
+{
+  private readonly OctokitApiClient apiClient = new(secretSettings.Value.GitHubAccessToken);
+
+  /// <inheritdoc/>
+  public async Task<List<RawCommitDetails>> ProcessAsync(long repositoryId, string fromReference, string toReference)
+  {
+    var getRawCommitsTask = this.GetRawCommitsFromOctokitAsync(repositoryId, fromReference, toReference);
+    var getRawCommitsResponse = await getRawCommitsTask.ConfigureAwait(false);
+
+    var domainResponse = MapRawCommitDetails(getRawCommitsResponse);
+    return domainResponse;
+  }
+
+  private async Task<Octokit.CompareResult> GetRawCommitsFromOctokitAsync(long repositoryId, string fromReference, string toReference)
+  {
+    // Get the specified repository's commits from Octokit
+    var getRepositoryCommitsOctokitTask = this.apiClient.Repository.Commit.Compare(repositoryId, @base: fromReference, head: toReference);
+    var getRepositoryCommitsOctokitResponse = await getRepositoryCommitsOctokitTask.ConfigureAwait(false);
+
+    return getRepositoryCommitsOctokitResponse;
+  }
+
+  private static List<RawCommitDetails> MapRawCommitDetails(Octokit.CompareResult getRawCommitsResponse)
+  {
+    // Takes the raw Octokit commit details and maps them to the domain model
+    var rawCommitDetails = getRawCommitsResponse.Commits.Select(rawCommit => new RawCommitDetails
+    {
+      Hash = rawCommit.Sha,
+      AuthorName = rawCommit.Author.Login,
+      AuthorEmail = rawCommit.Commit.Author.Email,
+      Message = rawCommit.Commit.Message,
+      DateOfCommit = rawCommit.Commit.Author.Date.Date,
+      IsMergeCommit = rawCommit.CheckIsMergeCommit()
+    }).ToList();
+
+    return rawCommitDetails;
+  }
+}
---
+++ tool-git-diffs-alternative-design/Common.Shared/Services/Commits/GetRawCommits/IGetRawCommitsDomainService.cs
@@ -0,0 +1,18 @@
+﻿namespace Common.Shared.Services.Commits.GetRawCommits;
+
+using Common.Shared.Models.Commits;
+
+/// <summary>
+/// A re-usable domain service responsible for retrieving raw commits for a single specified repository
+/// </summary>
+public interface IGetRawCommitsDomainService
+{
+  /// <summary>
+  /// Processes the provided details to fetch raw (unprocessed) commits for a single repository
+  /// </summary>
+  /// <param name="repositoryId">The repository id as retrieved from the Octokit Api</param>
+  /// <param name="fromReference">The git branch or tag to generate the diffs from</param>
+  /// <param name="toReference">The git branch or tag to generate the diffs until</param>
+  /// <returns>A list of raw (unprocessed) commits for a specified repository</returns>
+  public Task<List<RawCommitDetails>> ProcessAsync(long repositoryId, string fromReference, string toReference);
+}
