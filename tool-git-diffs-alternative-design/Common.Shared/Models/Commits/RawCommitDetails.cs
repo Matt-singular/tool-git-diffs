9a1c88771fd59490b93a05a82f07ed13c66da3de
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2024-11-25 21:11:21 +0200

    Created the RawCommitDetails common model

--- tool-git-diffs-alternative-design/Common.Shared/Models/Commits/RawCommitDetails.cs
+++ tool-git-diffs-alternative-design/Common.Shared/Models/Commits/RawCommitDetails.cs
@@ -1,9 +1,37 @@
 ﻿namespace Common.Shared.Models.Commits;
 
 /// <summary>
-/// Contains the raw commit details.
+/// Contains the raw commit details
 /// </summary>
 public class RawCommitDetails
 {
-  public RawCommitDetails() => throw new NotImplementedException();
+  /// <summary>
+  /// The commit's unique hash
+  /// </summary>
+  public string Hash { get; internal set; } = null!;
+
+  /// <summary>
+  /// The author of the commit
+  /// </summary>
+  public string AuthorName { get; internal set; } = null!;
+
+  /// <summary>
+  /// The email (possibly obfuscated) of the commit author
+  /// </summary>
+  public string AuthorEmail { get; internal set; } = null!;
+
+  /// <summary>
+  /// The message associated with the commit
+  /// </summary>
+  public string Message { get; internal set; } = null!;
+
+  /// <summary>
+  /// Whether it is a merge commit
+  /// </summary>
+  public bool IsMergeCommit { get; internal set; }
+
+  /// <summary>
+  /// The Date/Time the commit was made
+  /// </summary>
+  public DateTime DateOfCommit { get; internal set; }
 }
