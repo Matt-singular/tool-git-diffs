8736bc068b4ced5b4f9f0721bc3ea8b71129b0ba
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2024-11-26 22:04:14 +0200

    Implemented the GetCleanedCommits domain service

--- tool-git-diffs-alternative-design/Common.Shared/Services/Commits/GetCleanedCommits/GetCleanedCommitsDomainService.cs
+++ tool-git-diffs-alternative-design/Common.Shared/Services/Commits/GetCleanedCommits/GetCleanedCommitsDomainService.cs
@@ -2,16 +2,24 @@
 
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Common.Shared.Config;
+using Common.Shared.Extensions.Config;
 using Common.Shared.Models.Commits;
+using Microsoft.Extensions.Options;
 
 /// <inheritdoc cref="IGetCleanedCommitsDomainService"/>
-public class GetCleanedCommitsDomainService : IGetCleanedCommitsDomainService
+public class GetCleanedCommitsDomainService(IOptions<CommitSettings> commitSettings) : IGetCleanedCommitsDomainService
 {
+  private List<Regex> jiraReferencePatterns = null!;
+
   /// <inheritdoc />
   public List<CleanedCommitDetails> Process(List<RawCommitDetails> rawCommits)
   {
     ArgumentNullException.ThrowIfNull(rawCommits, nameof(rawCommits));
+    ArgumentNullException.ThrowIfNull(commitSettings.Value, nameof(commitSettings));
 
+    this.jiraReferencePatterns = commitSettings.Value.CreateRegexes();
     var cleanedCommits = rawCommits.Select(MapCleanedCommitDetails).ToList();
 
     return cleanedCommits;
@@ -19,6 +27,22 @@ public class GetCleanedCommitsDomainService : IGetCleanedCommitsDomainService
 
   private CleanedCommitDetails MapCleanedCommitDetails(RawCommitDetails rawCommit)
   {
-    throw new NotImplementedException();
+    var cleanedCommitDetail = new CleanedCommitDetails(rawCommit);
+    var priority = 0;
+
+    foreach (var regex in this.jiraReferencePatterns)
+    {
+      var references = regex.Matches(rawCommit.Message).Select(match => new CommitReference
+      {
+        JiraReference = match.Value,
+        Priority = priority
+      }).ToList();
+
+      cleanedCommitDetail.JiraReferences.AddRange(references);
+
+      priority++;
+    }
+
+    return cleanedCommitDetail;
   }
 }
