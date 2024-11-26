cddb11ac5de26622cb8ec40f3071fe4ad58f55a3
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2024-11-26 20:01:58 +0200

    Added summary comments and included additional commit details

--- tool-git-diffs-alternative-design/Common.Shared/Config/CommitSettings.cs
+++ tool-git-diffs-alternative-design/Common.Shared/Config/CommitSettings.cs
@@ -5,4 +5,31 @@
 /// </summary>
 public class CommitSettings
 {
+  /// <summary>
+  /// The different commit pattern details
+  /// </summary>
+  public Pattern[]? Patterns { get; set; }
+
+  /// <summary>
+  /// The sort order in terms of the different pattern headers
+  /// </summary>
+  public string[]? SortOrder { get; set; }
+
+  public class Pattern
+  {
+    /// <summary>
+    /// The regex to identify the commit reference
+    /// </summary>
+    public string? Regex { get; set; }
+
+    /// <summary>
+    /// The sorting priority for the specific commit reference
+    /// </summary>
+    public int? Priority { get; set; }
+
+    /// <summary>
+    /// The header for the commit reference to group under
+    /// </summary>
+    public string? Header { get; set; }
+  }
 }
--- tool-git-diffs-alternative-design/Common.Shared/Config/SecretSettings.cs
+++ tool-git-diffs-alternative-design/Common.Shared/Config/SecretSettings.cs
@@ -5,7 +5,18 @@
 /// </summary>
 public class SecretSettings
 {
+  /// <summary>
+  /// The personal access token to authenticate GitHub's Octokit API
+  /// </summary>
   public string? GitHubAccessToken { get; set; }
+
+  /// <summary>
+  /// The GitHub organisation's name
+  /// </summary>
   public string? GitHubOrganisationName { get; set; }
+
+  /// <summary>
+  /// The GitHub repository names
+  /// </summary>
   public string[]? GitHubRepositories { get; set; }
 }
