c78c436553240b896dae43c3de124aab86ca88c3
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2024-11-27 19:07:09 +0200

    Updated the Application.GUI startup logic

--- tool-git-diffs-alternative-design/Application.GUI/MauiProgram.cs
+++ tool-git-diffs-alternative-design/Application.GUI/MauiProgram.cs
@@ -1,24 +1,47 @@
-﻿using Microsoft.Extensions.Logging;
+﻿namespace Application.GUI;
 
-namespace Application.GUI;
+using Common.Shared.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+  public static MauiApp CreateMauiApp()
+  {
+    var builder = MauiApp.CreateBuilder();
 
+    // Configurations
+    builder.Configuration.AddCommonSharedConfiguration();
+    builder.Services.ConfigureCommonSettings(builder.Configuration);
+
+    // Dependency Injection
+    builder.Services.AddMauiPages();
+    builder.Services.AddCommonSharedServices();
+
+    // Maui App
+    builder
+      .UseMauiApp<App>()
+      .ConfigureFonts(fonts =>
+      {
+        fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+        fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+      });
+
+    // Logging
 #if DEBUG
-		builder.Logging.AddDebug();
+    builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
-	}
+    // Application Startup
+    var app = builder.Build();
+
+    return app;
+  }
+
+  private static IServiceCollection AddMauiPages(this IServiceCollection services)
+  {
+    services.AddSingleton<MainPage>();
+
+    return services;
+  }
 }
