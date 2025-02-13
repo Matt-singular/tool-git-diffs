490a5242a31b3d42f8f27ddc405f2435266875b9
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2025-02-13 19:15:44 +0200

    Added the Business.Domain project

---
+++ Business.Domain/Extensions/StartupExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Business.Domain.Extensions;
+
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Business.Domain startup extension methods
+/// </summary>
+[ExcludeFromCodeCoverage]
+public static class StartupExtensions
+{
+  /// <summary>
+  /// Adds Business.Domain services to the service collection
+  /// </summary>
+  /// <param name="services">The application's service collection</param>
+  /// <returns>The service collection with Business.Domain services registered</returns>
+  public static IServiceCollection AddBusinessDomainServices(this IServiceCollection services)
+  {
+    // Register Business.Domain services
+
+    return services;
+  }
+}
